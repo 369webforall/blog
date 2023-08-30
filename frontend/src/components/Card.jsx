c3f65abf93c3a3fa3000af4abe9a97069bccdea6
369webforall <369webforall@gmail.com>
2023-08-30 18:30:36 +0545

    update

--- frontend/src/components/Card.jsx
+++ frontend/src/components/Card.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
+
 export default function Card({ id, title, description, image, isUser }) {
   const navigate = useNavigate();
   console.log(isUser);
